2e59bd7624b8c911687c7a787d40a64cbbeccfaa
恪晨 <bo.wang1016@outlook.com>
2020-04-07 14:19:30 +0800

    chore(): :wrench: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,23 +9,24 @@ module.exports = {
 	rules: {
 		'type-enum': [2, 'always',
 			[
-				'feat', // 新功能（feature）
-				'fix', // 修补bug
-				'docs', // 文档（documentation）
-				'style', // 格式（不影响代码运行的变动）
-				'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
-				'test', // 增加测试
-				'revert', // 回滚
-				'config', // 构建过程或辅助工具的变动
-				'chore', // 其他改动
+				'build',
+				'ci',
+				'chore',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'style',
+				'test'
 			],
 		],
-		'type-case': [0],
-		'type-empty': [0],
-		'scope-empty': [0],
-		'scope-case': [0],
+		'type-case': [0, 'always'], // 提交类型
+		'type-empty': [0], // 提交类型不能为空
 		'subject-full-stop': [0, 'never'],
-		'subject-case': [0, 'never'],
+		'subject-case': [0, 'never'], // 提交信息格式
+		'subject-empty': [0, 'never'], // 提交信息不能为空
 		'header-max-length': [0, 'always', 72]
 	}
 };
